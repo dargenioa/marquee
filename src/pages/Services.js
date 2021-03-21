b349226d49b9174f2752c5533d71e4452d736f00
Amanda D'Argenio <dargenioa14@gmail.com>
2021-03-21 12:29:02 -0400

    returning data from json file using state

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,13 +1,49 @@
+import React, { useEffect, useState } from "react";
+// import Pages from "../utils/content.json";
 import Header from "../components/layout/Header/Header";
 
 function Services() {
-    return (
-        <>
-         <div className="Slide-One">
-         <Header/>
-             </div>
-        </>
-    );
+  const [backgroundImage, setBackgroundImage] = useState();
+  const [cta, setCta] = useState();
+  const [headline, setHeadline] = useState();
+  const [subhead, setSubhead] = useState();
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    // if (window.location.pathname === "/services") {
+    //     fetch("../utils/content.json")
+    //     .then((res) => console.log(res.data.));
+    //     }
+    // }
+
+    getData();
+
+  }, []);
+
+  const getData = () => {
+    fetch("./data.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then(function (response) {
+        console.log(response);
+        return response.json();
+      })
+      .then(function (myJson) {
+        console.log(myJson);
+        setData(myJson);
+      });
+  };
+
+  return (
+    <>
+      <div className="Slide-One">
+        <Header />
+      </div>
+    </>
+  );
 }
 
 export default Services;
