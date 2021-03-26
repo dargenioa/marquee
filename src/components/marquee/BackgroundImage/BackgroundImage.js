ea80e4e7042c758c785124bd649ca62c3959d698
Amanda D'Argenio <dargenioa14@gmail.com>
2021-03-25 20:19:49 -0400

    updated services page with rendering states and background image

---
+++ src/components/marquee/BackgroundImage/BackgroundImage.js
@@ -0,0 +1,11 @@
+function BackgroundImage(props) {
+  return (
+      <div
+      className="Slide-one"
+      style={{backgroundImage: 'url("https://mdbootstrap.com/docs/react/css/background-image/")'}}
+      >
+      </div>
+  );
+}
+
+export default BackgroundImage;
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 // import Pages from "../utils/content.json";
 import Header from "../components/layout/Header/Header";
+import BackgroundImage from "../components/marquee/BackgroundImage/BackgroundImage";
 
 function Services() {
   const [backgroundImage, setBackgroundImage] = useState();
@@ -10,15 +11,16 @@ function Services() {
   const [data, setData] = useState();
 
   useEffect(() => {
-    // if (window.location.pathname === "/services") {
-    //     fetch("../utils/content.json")
-    //     .then((res) => console.log(res.data.));
-    //     }
-    // }
 
     getData();
 
   }, []);
+
+ const setBackground = () => {
+    if (window.location.pathname === "/services") {
+        console.log(data);
+        }
+  }
 
   const getData = () => {
     fetch("./data.json", {
@@ -31,17 +33,27 @@ function Services() {
         console.log(response);
         return response.json();
       })
-      .then(function (myJson) {
-        console.log(myJson);
-        setData(myJson);
+      .then(function (data) {
+        console.log(data.pages[1]);
+        setData(data.pages[1])
+        setBackgroundImage(data.pages[1].blocks[0].background);
+        setHeadline(data.pages[1].blocks[0].headline);
+        setSubhead(data.pages[1].blocks[0].subhead);
       });
   };
 
+  // const image1 = data[0].blocks[0].background;
+
+
   return (
     <>
-      <div className="Slide-One">
-        <Header />
-      </div>
+          <div
+          className="Slide-One"
+          style={{ 
+          backgroundImage: `url(${backgroundImage})`}}
+    >
+      <Header />
+    </div>
     </>
   );
 }
